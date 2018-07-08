d5e906edd8b135202deaf6279e5dd386bab7653c
Rizki Syarif <Rizkibizniz@rizkis-macbook-air.home>
2018-07-08 19:47:27 +0200

    shelter/designs/components/loss_functions.py

--- shelter/designs/components/loss_functions.py
+++ shelter/designs/components/loss_functions.py
@@ -19,18 +19,3 @@ def dice_coef(y_true, y_pred):
 # loss is negative dice coefficient
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
-
-
-# # dice coefficient
-# def dice_coef(truth, prediction):
-#     truth = K.flatten(truth)
-#     prediction = K.flatten(prediction)
-#     truth = np.array(truth)
-#     prediction = np.array(prediction)
-#     print(prediction)
-#     n_true_positive  = sum(prediction[truth == 1])
-#     n_false_positive = sum(prediction[truth == 0])
-#     n_false_negative = sum(1-prediction[truth == 1])
-#     dice = 2 * n_true_positive / (2*n_true_positive + n_false_positive + n_false_negative)
-#     tf.convert_to_tensor(dice, np.float32)
-#     return(dice)
