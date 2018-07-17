04aca34b7c5218eb64d3f224deb2104dca42299b
Rizki Syarif <Rizkibizniz@Rizkis-MacBook-Air.local>
2018-07-17 11:16:00 +0900

    tidy up

--- shelter/designs/components/loss_functions.py
+++ shelter/designs/components/loss_functions.py
@@ -1,21 +1,20 @@
 from keras import backend as K 
 # import numpy as np
 
+
+# dice coefficient: value is between 0 and 1 
+# (see: https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient)
+# Closer to 0 means two distributions are very dissimilar, 1 the distributions are perfectly identical.
+# returns ~0 for all wrong
+# returns ~0 for prediction all 0
+# returns 1 for all correct
+# penalizes false positives
+# The smooth is inserted to avoid having value of exactly 0, and they are inserted in both in denom and num so max value is still 1.
 smooth = 1.0
-
-# # dice coefficient
 def dice_coef(y_true, y_pred):
-    # truth as vector:
-
-    # count predicted "1"s that are also true "1"s = count true positives
     intersection = K.sum(y_true * y_pred)
-    # 2 * count true positives / ( count true "1"s + count predicted "1"s
-    # returns 0 for all wrong
-    # returns 0 for prediction all 0
-    # returns 1 for all correct
-    # how 'good' a value in between depends is on total % of true "1"s.
     return (2. * intersection + smooth) / (K.sum(y_true) + K.sum(y_pred) + smooth)
 
-# loss is negative dice coefficient
+# loss is to be minimized so we take the negative of the dice coefficient
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
